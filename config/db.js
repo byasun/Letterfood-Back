bfee1e93bf0670fd6110554930547d43fa2183cb
byasun <annaclarag6@gmail.com>
2024-11-28 18:53:35 -0300

    pequenas correções

--- config/db.js
+++ config/db.js
@@ -2,14 +2,15 @@ const mongoose = require('mongoose');
 
 const conectarDB = async () => {
   try {
-      await mongoose.connect(process.env.MONGO_URI);
-      console.log('Conexão com o MongoDB estabelecida com sucesso.');
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log('Conexão com o MongoDB estabelecida com sucesso.');
   } catch (error) {
-      console.log('Erro ao conectar ao MongoDB:', error);
-      process.exit(1);
+    console.error('Erro ao conectar ao MongoDB:', error.message);
+    process.exit(1); // Termina o processo em caso de falha na conexão
   }
 };
 
+// Define opções globais (opcional, mas útil em desenvolvimento)
 mongoose.set('debug', process.env.NODE_ENV === 'development');
 
 module.exports = conectarDB;
